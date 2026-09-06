cb46f4520b7564464fa81ef06170c3fd206216f0
Mani Tofigh <manitofigh@protonmail.com>
2026-09-06 16:35:27 -0400

    fix: track latest underleaf main

--- lua/plugins/underleaf.lua
+++ lua/plugins/underleaf.lua
@@ -5,6 +5,7 @@ return {
     "manitofigh/Underleaf",
     dir = vim.fn.isdirectory(checkout) == 1 and checkout or nil,
     branch = "main",
+    version = false, -- Follow main rather than pinning a release tag.
     opts = { browser = "chrome" },
   },
 }
